0331c4bce9810599067c4bb46fdd3ccd20c4c7b8
juan3237 <eljuan3237@gmail.com>
2016-04-19 00:48:57 -0700

    aqui esta el esqueleto de la app

--- PastoralAdmin/PastoralAdmin/Global.asax.cs
+++ PastoralAdmin/PastoralAdmin/Global.asax.cs
@@ -11,8 +11,6 @@ namespace PastoralAdmin
     {
         protected void Application_Start()
         {
-            //aqui huo un cambio
-
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
